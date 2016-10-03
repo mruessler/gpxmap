9314be9814c68d56387bd4a91b7007335f93a7c8
Martin Rüßler <martin.ruessler@mailbox.org>
2016-10-03 20:25:03 +0200

    Updated plotgpx.
    Improved the code, switched to ggmap functions.

--- plotgpx.R
+++ plotgpx.R
@@ -2,6 +2,7 @@ plotgpx <- function(folder, type = "osm", zoom = 14, col = "purple", mapborder =
 	#' plotgpx
 	#'
 	#' @description Create a map of GPS data
+	#' @param folder folder where the GPS data are stored
 	#' @references help and infos from:
 	#' http://dbsgeo.com/latlon/
 	#' http://www.r-bloggers.com/plot-maps-like-a-boss/
@@ -11,51 +12,32 @@ plotgpx <- function(folder, type = "osm", zoom = 14, col = "purple", mapborder =
 	#' A1 paper: 594 * 841 mm
 
 	# load the required libraries
-	source(file = "loadgpxlibs.r")
-   source(file = "readgpxtracks.r")
-   source(file = "plotgpxtracks.r")
+	source(file = "loadgpxlibs.R")
+	source(file = "loadmeerun.R")
+	source(file = "plotgpxtracks.R")
 	loadgpxlibs()
 
 	# load the gpx data
-	# get infos via ogrListLayers(object) or ogrListLayers("filename")
-	gpx <- readgpxtracks(folder = folder)
+	gpx <- loadmeerun(folder = folder)
+	# set map boundaries
+	lon <- extendrange(c(min(gpx$longitude), max(gpx$longitude)), f = mapborder)
+	lat <- extendrange(c(min(gpx$latitude), max(gpx$latitude)), f = mapborder)
 
-	# set the map area; needs to be automated
-	# create a data frame which holds all min and max lat/lon values
-	minmax <- data.frame(xmin = vector(mode = "numeric", length = length(gpx)),
-											 xmax = vector(mode = "numeric", length = length(gpx)),
-											 ymin = vector(mode = "numeric", length = length(gpx)),
-											 ymax = vector(mode = "numeric", length = length(gpx)))
+	corners <- data.frame(tl = c(lon[1], lat[2]),
+											  tr = c(lon[2], lat[2]),
+											  bl = c(lon[1], lat[1]),
+											  br = c(lon[2], lat[1]),
+												row.names = c("longitude", "latitude"))
 
-											 # fill the data frame with the min and max values
-	for (i in 1:length(gpx)) {
-		# x values
-		minmax[i,1:2] <- summary(gpx[[i]])[[2]][1,]
-		# y values
-		minmax[i,3:4] <- summary(gpx[[i]])[[2]][2,]
-	}
-
-	# get edges: two lat / lon pairs. lat == y, lon == x
-	tl <- c(max(minmax[, 3:4]), min(minmax[, 1:2]))
-	br <- c(min(minmax[, 3:4]), max(minmax[, 1:2]))
-	# create a data frame with lat and lon data
-	coords <- data.frame(cbind(tl, br), row.names = c("lat", "lon"))
 	# increase the range by a given factor
-	coords[1, ] <- extendrange(coords[1, ], f = mapborder)
-	coords[2, ] <- extendrange(coords[2, ], f = mapborder)
-
-	# height and width of the map
-	hw <- abs(tl - br)
+	# corners <- grDevices::extendrange(corners, f = mapborder)
 
 	# download map tiles
-	map <- openproj(openmap(tl, br, zoom = zoom, type = type), projection = "+proj=longlat")
-
+	map <- ggmap::get_map(location = c(corners$bl, corners$tr), source = "stamen", maptype = "toner", zoom = 13, scale = "auto", messaging = FALSE)
 	# create a pdf file
 	pdf(file = "Map.pdf", title = filetitle, width = 19.69, height = 27.95)
-	plot(map)
-
-	# plot gpx tracks
-	plotgpxtracks(gpx, colour = col)
-
+	ggmap(map, extent = "device") +
+		geom_path(aes(x = longitude, y = latitude, group = factor(track)),
+		colour = "purple", data = gpx, alpha = 0.3, size = 1)
 	dev.off()
 }
