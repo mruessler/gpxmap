fb5eb4bd465b700a836f2afebe955adbd4358d7a
Martin Rüßler <martin.ruessler@mailbox.org>
2017-01-02 21:56:47 +0100

    Add all the changes needed for a proper package creation.
    - updated DESCRIPTION and NAMESPACE files
    - updated package comments and manual files

--- R/loadrunkeeper.R
+++ R/loadrunkeeper.R
@@ -1,6 +1,11 @@
+#' loadrunkeeper
+#'
+#' @description Load gpx files and return a tidy data frame (function adapted from FlowingData).
+#' @param folder select the folder containing the kml file(s)
+#' @return gpx tidy data frame containing loaded data
+#' @export
+#'
 loadrunkeeper <- function(folder) {
-	library(plotKML)
-
 	# GPX files downloaded from Runkeeper
 	files <- dir(folder, pattern = "\\.gpx")
 
@@ -9,21 +14,20 @@ loadrunkeeper <- function(folder) {
 	latitude <- c()
 	longitude <- c()
 	for (i in 1:length(files)) {
-
-		route <- readGPX(files[i])
+		route <- plotKML::readGPX(files[i])
 		location <- route$tracks[[1]][[1]]
 
 		index <- c(index, rep(i, dim(location)[1]))
 		latitude <- c(latitude, location$lat)
 		longitude <- c(longitude, location$lon)
 	}
-	routes <- data.frame(cbind(index, latitude, longitude))
-
+	gpx <- data.frame(cbind(index, latitude, longitude))
+  return(gpx)
 	# Map the routes
-	ids <- unique(index)
-	plot(routes$longitude, routes$latitude, type="n", axes=FALSE, xlab="", ylab="", main="", asp=1)
-	for (i in 1:length(ids)) {
-		currRoute <- subset(routes, index==ids[i])
-		lines(currRoute$longitude, currRoute$latitude, col="#00000020")
-	}
+	# ids <- unique(index)
+	# plot(routes$longitude, routes$latitude, type = "n", axes = FALSE, xlab = "", ylab = "", main = "", asp = 1)
+	# for (i in 1:length(ids)) {
+	# 	currRoute <- subset(routes, index == ids[i])
+	# 	lines(currRoute$longitude, currRoute$latitude, col = "#00000020")
+	# }
 }
--- R/plotgpx.R
+++ R/plotgpx.R
@@ -1,27 +1,27 @@
-plotgpx <- function(folder, type = "osm", zoom = 14, col = "purple", mapborder = 0.1, filetitle = "Map plot") {
-	#' plotgpx
-	#'
-	#' @description Create a map of GPS data
-	#' @param folder folder where the GPS data are stored
-	#' @references help and infos from:
-	#' http://dbsgeo.com/latlon/
-	#' http://www.r-bloggers.com/plot-maps-like-a-boss/
-	#' http://stackoverflow.com/questions/13762793/plotting-choropleth-maps-from-kml-data-using-ggplot2
-	#' http://flowingdata.com/2011/05/11/how-to-map-connections-with-great-circles/
-	#' http://rpsychologist.com/parsing-data-from-a-text-file-and-plotting-where-people-live-using-ggplot2-and-openstreetmaps
-	#' A1 paper: 594 * 841 mm
+#' plotgpx
+#'
+#' @description Create a map with GPS tracks using map parameters extracted from used tracks
+#' @param folder select the folder containing the kml file(s)
+#' @param type the type of map
+#' @param zoom zoom level of the map
+#' @param col colour of the GPS tracks
+#' @param mapborder ratio by which the height and width of the map are increased
+#' @param pdftitle title of the resulting pdf file
+#' @return kml tidy data frame containing loaded data
+#' @export
+#' @references help and infos from:
+#' - http://dbsgeo.com/latlon/
+#' - http://www.r-bloggers.com/plot-maps-like-a-boss/
+#' - http://stackoverflow.com/questions/13762793/plotting-choropleth-maps-from-kml-data-using-ggplot2
+#' - http://flowingdata.com/2011/05/11/how-to-map-connections-with-great-circles/
+#' - http://rpsychologist.com/parsing-data-from-a-text-file-and-plotting-where-people-live-using-ggplot2-and-openstreetmaps
 
-	# load the required libraries
-	source(file = "loadgpxlibs.R")
-	source(file = "loadmeerun.R")
-	source(file = "plotgpxtracks.R")
-	loadgpxlibs()
-
+plotgpx <- function(folder, type = "osm", zoom = 14, col = "purple", mapborder = 0.1, pdftitle = "Map plot") {
 	# load the gpx data
 	gpx <- loadmeerun(folder = folder)
 	# set map boundaries
-	lon <- extendrange(c(min(gpx$longitude), max(gpx$longitude)), f = mapborder)
-	lat <- extendrange(c(min(gpx$latitude), max(gpx$latitude)), f = mapborder)
+	lon <- grDevices::extendrange(c(min(gpx$longitude), max(gpx$longitude)), f = mapborder)
+	lat <- grDevices::extendrange(c(min(gpx$latitude), max(gpx$latitude)), f = mapborder)
 
 	corners <- data.frame(tl = c(lon[1], lat[2]),
 											  tr = c(lon[2], lat[2]),
@@ -35,9 +35,9 @@ plotgpx <- function(folder, type = "osm", zoom = 14, col = "purple", mapborder =
 	# download map tiles
 	map <- ggmap::get_map(location = c(corners$bl, corners$tr), source = "stamen", maptype = "toner", zoom = 13, scale = "auto", messaging = FALSE)
 	# create a pdf file
-	pdf(file = "Map.pdf", title = filetitle, width = 19.69, height = 27.95)
-	ggmap(map, extent = "device") +
-		geom_path(aes(x = longitude, y = latitude, group = factor(track)),
+	grDevices::pdf(file = "Map.pdf", title = pdftitle, width = 19.69, height = 27.95)
+	ggmap::ggmap(map, extent = "device") +
+		ggplot2::geom_path(ggplot2::aes(x = longitude, y = latitude, group = factor(track)),
 		colour = "purple", data = gpx, alpha = 0.3, size = 1)
-	dev.off()
+	grDevices::dev.off()
 }
